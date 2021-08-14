fa306c3115f01d64100370a24f53199fd09e806a
Shakti  Tripathi <shaktitripathi02@gmail.com>
2021-08-15 02:23:51 +0530

    Node http

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
-var rect = require('./rectangle');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const hostname= 'localhost';
+const port = 3001;
 
-function solveRect(l,b){
- console.log("Solving for rectangle")
- rect(l,b,(err,rectangle)=>{
-     if(err){
-         console.log(err.message);
-     }
-     else{
-         console.log("Area is" ,rectangle.area());
-         console.log("Perimeter is ",rectangle.perimeter());
-     }
 
- })
 
+const server = http.createServer((req,res)=>{
+  console.log('Request for ' + req.url + ' by method ' + req.method);
+  if(req.method == "GET"){
+      var fileUrl;
+      if(req.url == '/'){
+        fileUrl = '/index.html';
+      }
+      else{
+          fileUrl = req.url;
+      }
+      var filePath = path.resolve('./public'+fileUrl);
+      const fileExt = path.extname(filePath);
+      if(fileExt == '.html'){
+        fs.exists(filePath, (exists) => {
+            if (!exists) {
+              res.statusCode = 404;
+              res.setHeader('Content-Type', 'text/html');
+              res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                          ' not found</h1></body></html>');
+              return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            fs.createReadStream(filePath).pipe(res);
+          });
+      }
+      else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                ' not a HTML file</h1></body></html>');
+      }
+    
+  }
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Error 404: ' + req.method + 
+            ' not supported</h1></body></html>');
 }
+  //res.statusCode = 200;
+  //res.setHeader('Content-Type','text/html');
+  //res.end('<html><body><h1>Hello World</h1></body> </html>')
+})
 
-solveRect(5,9)
-solveRect(-3,4);
+
+server.listen(port,hostname,()=>{
+     console.log(`Server is running on ${port}`);
+})
